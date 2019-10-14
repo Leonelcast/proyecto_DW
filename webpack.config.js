557cc4f44f2aeaf61e7791815be52e82b2118144
Leonelcast <Leonelcas.izq@gmail.com>
2019-10-14 16:28:28 -0600

    agregando build de webpack

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports =
+    {
+        entry: './js/app.js',
+        output: {
+            path: __dirname + '/build',
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+              {
+                test: /\.s[ac]ss$/i,
+            use: [
+          
+              MiniCssExtractPlugin.loader,
+             
+              'css-loader',
+             
+              'sass-loader',
+                ],
+              },
+            ],
+          },
+        plugins: [
+          new MiniCssExtractPlugin({
+            filename: 'style.css'
+          }),
+          new HtmlWebpackPlugin({
+            filename: 'adminUser.html',
+            template: 'adminUser.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'articuloApple.html',
+            template: 'ArticuloApple.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'articuloNintendo.html',
+            template: 'articuloNintendo.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'articuloTesla.html',
+            template: 'articuloTesla.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'authorUser.html',
+            template: 'authorUser.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'categoriaX.html',
+            template: 'categoriaX.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'categoriaY.html',
+            template: 'categoriaY.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'crearArticulo.html',
+            template: 'crearArticulo.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'crearArticulo2.html',
+            template: 'crearArticulo2.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'crearArticulo3.html',
+            template: 'crearArticulo3.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'login.html',
+            template: 'login.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'loginUser.html',
+            template: 'loginUser.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'moderatorUser.html',
+            template: 'moderatorUser.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'moduloModerator.html',
+            template: 'moduloModerator.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'subCategoriaX.html',
+            template: 'subCategoriax.html'
+          }), 
+          new HtmlWebpackPlugin({
+            filename: 'subCategoriaY.html',
+            template: 'subCategoriaY.html'
+          }), 
+          
+         
+        ]
+    }
